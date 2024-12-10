9551c861958c19c0b1e76477d8062549c7819954
Rabieh Sassi <rabieh.sassi@epitech.eu>
2024-12-10 10:37:15 +0000

    Modified name of the project

--- backend/src/test/java/com/devops/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/devops/backend/BackendApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-class DemoApplicationTests {
+class BackendApplicationTests {
     @Autowired
     private ApplicationContext context;
 
