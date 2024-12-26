95d1bfe9c435ad098a09f293e54f3a5664867bab
Noah Daën <daennoah@gmail.com>
2024-12-26 19:51:08 +0100

    feat: add web icon

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,22 +1,23 @@
-import type { Metadata } from "next";
+import type {Metadata} from "next";
 import "@/styles/globals.css";
 import React from "react";
 
 export const metadata: Metadata = {
-  title: "Trinity",
-  description: "La plateforme de gestion de vos produits, factures et clients",
+    title: "Trinity",
+    description: "La plateforme de gestion de vos produits, factures et clients",
+    icons: 'images/web-icon-trinity.svg',
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body>
+    return (
+        <html lang="en">
+        <body>
         {children}
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
