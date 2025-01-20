95e30a921541be9a9e79df9387de1f9daaed72b1
Gabriel Lopez <gabriel.lopez@epitech.eu>
2025-01-20 20:57:52 +0100

    ✨ Add AuthenticationController
    for user registration and login

---
+++ backend/src/main/java/com/trinity/auth/controller/AuthenticationController.java
@@ -0,0 +1,37 @@
+package com.trinity.auth.controller;
+
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.trinity.auth.dto.AuthenticationResponse;
+import com.trinity.auth.dto.LoginRequest;
+import com.trinity.auth.dto.RegisterRequest;
+import com.trinity.auth.service.AuthenticationService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@Valid @RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> login(@Valid @RequestBody LoginRequest request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+    
+    
+}
