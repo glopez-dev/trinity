ddb8cf8086547d40881bb85b42cfbfad30fdce4c
Gabriel Lopez <gabriel.lopez@epitech.eu>
2025-01-25 18:43:28 +0100

    ♻️ Rename employee migration file and change id type from BIGSERIAL to UUID

--- backend/src/main/resources/db/migration/V3__CREATE_EMPLOYEE.sql
+++ backend/src/main/resources/db/migration/V3__CREATE_EMPLOYEE.sql
@@ -1,6 +1,6 @@
 CREATE TABLE employee
 (
-    id               BIGSERIAL PRIMARY KEY,
+    id               UUID PRIMARY KEY,
     email            VARCHAR(255) NOT NULL,
     first_name       VARCHAR(255),
     last_name        VARCHAR(255),
