3f946afe81e1202a60e3889570a48147d11de1d5
Gabriel Lopez <gabriel.lopez@epitech.eu>
2025-01-27 15:51:17 +0100

    🐛 Add missing builder to ImagesUrls

--- backend/src/main/java/com/trinity/product/core/dto/ImageUrls.java
+++ backend/src/main/java/com/trinity/product/core/dto/ImageUrls.java
@@ -4,10 +4,16 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Embeddable
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ImageUrls {
     @Column(insertable = false, updatable = false)
