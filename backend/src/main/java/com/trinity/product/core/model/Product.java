0f68c993edb7efe4de4bdef90b9f157769cdff57
Gabriel Lopez <gabriel.lopez@epitech.eu>
2025-01-27 11:00:33 +0100

    ✨ Add Product entity with attributes and embedded classes

---
+++ backend/src/main/java/com/trinity/product/core/model/Product.java
@@ -0,0 +1,102 @@
+package com.trinity.product.core.model;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import com.trinity.product.core.dto.ImageUrls;
+
+@Embeddable
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String barcode;
+
+    private String category;
+
+    private String brand;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String ingredients;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @Embedded
+    private NutrientLevels nutrientLevels;
+
+    @Embeddable 
+    @Data
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class NutrientLevels {
+        private String fat;
+        private String saturatedFat;
+        private String sugars;
+        private String salt;
+    }
+
+    @Embedded
+    private Nutriments nutriments;
+
+    @Embeddable
+    @Data
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Nutriments {
+        private double energyKcal100g;
+        private double proteins100g;
+        private double carbohydrates100g;
+        private double fat100g;
+        private double fiber100g;
+        private double salt100g;
+        private double sugars100g;
+    }
+
+    private String nutriscoreGrade; 
+
+    @Embedded
+    private SelectedImages selectedImages;
+
+    @Embeddable
+    @Data
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class SelectedImages {
+
+        @Embedded
+        private ImageUrls display;
+ 
+        @Embedded
+        private ImageUrls small;
+ 
+        @Embedded
+        private ImageUrls thumb;
+    }
+
+    @LastModifiedDate
+    private LocalDateTime lastUpdate;
+}
