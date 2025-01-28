8257da0e6ce6ec1e985cef94ab1beccdd598e66b
Gabriel Lopez <gabriel.lopez@epitech.eu>
2025-01-28 15:46:49 +0000

    🎉 Init the "Stock" module

---
+++ backend/src/main/java/com/trinity/product/dto/TrinitySearchRequest.java
@@ -0,0 +1,17 @@
+package com.trinity.product.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TrinitySearchRequest {
+
+    @NotEmpty(message = "Search term must not be empty")
+    @Pattern(regexp = "^[a-zA-Z]+$", message = "Search term must contain only alphabetic characters")
+    private String searchTerm;
+}
---
+++ backend/src/main/java/com/trinity/product/dto/open_food_facts/OpenFoodFactsNutrientLevels.java
@@ -0,0 +1,22 @@
+package com.trinity.product.dto.open_food_facts;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class OpenFoodFactsNutrientLevels {
+    @JsonProperty("fat")
+    private String fat;
+
+    @JsonProperty("salt")
+    private String salt;
+
+    @JsonProperty("saturated-fat")
+    private String saturatedFat;
+    
+    @JsonProperty("sugars")
+    private String sugars;
+}
