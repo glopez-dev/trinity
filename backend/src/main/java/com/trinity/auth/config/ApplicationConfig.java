5f01f6aba8471abe67a5ef8a2a9a86487585f324
Gabriel Lopez <gabriel.lopez@epitech.eu>
2025-02-03 10:58:35 +0100

    🔥 Remove unused imports in ApplicationConfig

--- backend/src/main/java/com/trinity/auth/config/ApplicationConfig.java
+++ backend/src/main/java/com/trinity/auth/config/ApplicationConfig.java
@@ -1,10 +1,8 @@
 package com.trinity.auth.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
