56ae3a3bb0f32b98251a4db5af30c2407faf6f12
Gabriel Lopez <gabriel.lopez@epitech.eu>
2025-02-06 23:05:42 +0100

    🚑  HOTFIX: change static URI builder

--- backend/src/main/java/com/trinity/product/service/OpenFoodFactsService.java
+++ backend/src/main/java/com/trinity/product/service/OpenFoodFactsService.java
@@ -4,7 +4,7 @@ import java.net.URI;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.trinity.product.adapter.OpenFoodFactsAdapter;
 import com.trinity.product.dto.TrinitySearchResponse;
@@ -18,24 +18,21 @@ import lombok.AllArgsConstructor;
 public class OpenFoodFactsService {
 
     private final WebClient webClient;
-    private final UriBuilder uriBuilder;
 
     static final String FIELDS_TO_GET = "products,allergens_imported,allergens,code,brands,brand_imported,compared_to_category,grade,ingredients_text_fr,nutrient_levels,nutriments,product_name_fr_imported,quantity_imported,selected_images,nutriscore_grade,generic_name_fr,generic_name_en,ingredients_text_en"; 
     
-    URI buildUri(String searchTerm) {
-        return this.uriBuilder
-            .scheme("https")
-            .host("world.openfoodfacts.org")
-            .path("/cgi/search.pl")
-            .queryParam("search_terms", searchTerm)
-            .queryParam("fields", OpenFoodFactsService.FIELDS_TO_GET)
-            .queryParam("page_size", 10)
-            .queryParam("json", 1)
-            .queryParam("sort_by", "unique_scans_n")
-            .build();
+    public URI buildUri(String searchTerm) {
+        return UriComponentsBuilder.fromHttpUrl("https://world.openfoodfacts.org/cgi/search.pl")
+                .queryParam("search_terms", searchTerm)
+                .queryParam("fields", OpenFoodFactsService.FIELDS_TO_GET)
+                .queryParam("page_size", 10)
+                .queryParam("json", 1)
+                .queryParam("sort_by", "unique_scans_n")
+                .build()
+                .toUri();
     }
 
-    OpenFoodFactSearchResponse getSearchResponseJson(URI uri) {
+    public OpenFoodFactSearchResponse getSearchResponseJson(URI uri) {
         return webClient.get()
             .uri(uri)
             .retrieve()
