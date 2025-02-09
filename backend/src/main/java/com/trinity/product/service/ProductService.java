5acedac0f0a1562b6138cdf2e46cb207fa53bee4
Gabriel Lopez <gabriel.lopez@epitech.eu>
2025-02-09 18:13:18 +0100

    ✨ Implement updateProduct method in ProductService for updating product details

--- backend/src/main/java/com/trinity/product/service/ProductService.java
+++ backend/src/main/java/com/trinity/product/service/ProductService.java
@@ -8,10 +8,12 @@ import org.springframework.stereotype.Service;
 
 import com.trinity.product.dto.api.CreateProductDTO;
 import com.trinity.product.dto.api.ReadProductDTO;
+import com.trinity.product.dto.api.UpdateProductDTO;
 import com.trinity.product.mapper.ProductMapper;
 import com.trinity.product.model.Product;
 import com.trinity.product.repository.ProductRepository;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 
 @Service
@@ -46,6 +48,23 @@ public class ProductService {
         return productMapper.toDTO(product);
     }
 
+    public ReadProductDTO updateProduct(UUID productId, UpdateProductDTO request) {
+        Product product = productRepository.findById(productId)
+            .orElseThrow(() -> new RuntimeException("Product not found"));
 
+        request.getName().ifPresent(product::setName);
+        request.getPrice().ifPresent(product::setPrice);
+
+        request.getQuantity().ifPresent(quantity -> {
+            Product.Stock stock = product.getStock();
+            stock.setQuantity(stock.getQuantity() + quantity);
+            product.setStock(stock);
+        });
+
+
+        Product updatedProduct = productRepository.save(product);
+
+        return productMapper.toDTO(updatedProduct);
+    }
 
 }
